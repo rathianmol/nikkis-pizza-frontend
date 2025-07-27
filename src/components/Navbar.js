7a6568b385cb368502e8fcb027bd473d0bbd5006
rathianmol <rathianmol25@gmail.com>
2025-07-27 00:42:21 -0400

    Adding base-line componenets, establishing react-browser and links.

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/login">Login</Link>
+       <Link to="/order">Order</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
