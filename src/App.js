5075c2f64605dc250bc8b4a7767ace55399eb607
rathianmol <rathianmol25@gmail.com>
2025-08-23 16:14:33 -0400

    Adding an auth-gaurd for user-protected routes; implented with address component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import Home from './components/Home';
 import Login from './components/Login';
-import Menu from './components/Menu';
+// import Menu from './components/Menu';
 import Navbar from './components/Navbar';
 import Register from './components/Register';
 import Cart from './components/Cart';
@@ -12,7 +12,7 @@ import store from './redux/store';
 import Footer from './components/Footer';
 import { AuthProvider } from './contexts/AuthContext';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import ProtectedRoutes from './utils/ProtectedRoutes';
 import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -23,11 +23,14 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/pizzas" element={<Pizzas />} />
-            <Route path="/menu" element={<Menu />} />
+            {/* <Route path="/menu" element={<Menu />} /> */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/create-address" element={<UserAddress />} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/create-address" element={<UserAddress />} />
+            </Route>
+            {/* <Route path="/create-address" element={<UserAddress />} /> */}
           </Routes>
           <Footer />
         </AuthProvider>
---
+++ src/utils/ProtectedRoutes.js
@@ -0,0 +1,9 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+const ProtectedRoutes = () => {
+    const { isAuthenticated } = useAuth();
+    return isAuthenticated ? <Outlet/> : <Navigate to="/" />
+}
+
+export default ProtectedRoutes;
