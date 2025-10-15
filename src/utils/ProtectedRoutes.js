9c0fa6b576b5c6e128355962ff42d3b453495a68
rathianmol <rathianmol25@gmail.com>
2025-10-15 04:29:52 -0400

    Redirecting user to login, instead of home-page, for protected routes.

--- src/utils/ProtectedRoutes.js
+++ src/utils/ProtectedRoutes.js
@@ -3,7 +3,7 @@ import { useAuth } from '../contexts/AuthContext';
 
 const ProtectedRoutes = () => {
     const { isAuthenticated } = useAuth();
-    return isAuthenticated ? <Outlet/> : <Navigate to="/" />
+    return isAuthenticated ? <Outlet/> : <Navigate to="/login" />
 }
 
 export default ProtectedRoutes;
