a37fbc0cec6fc106dfb82fbe9bcb44fe5b6622e3
rathianmol <rathianmol25@gmail.com>
2025-10-16 08:20:54 -0400

    Fixing logic inside protected route for admin access.

--- src/utils/AdminProtectedRoutes.js
+++ src/utils/AdminProtectedRoutes.js
@@ -2,6 +2,7 @@ import { Outlet, Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 const AdminProtectedRoutes = () => {
+    // debugger;
     const { isAuthenticated, user } = useAuth();
     
     // Check if user is authenticated AND has admin role
@@ -9,12 +10,11 @@ const AdminProtectedRoutes = () => {
         return <Navigate to="/login" />;
     }
     
-    if (user?.role !== 'admin' || user?.role !== 'super-admin') {
-        // Redirect non-admin users to home
-        return <Navigate to="/" />;
+    if (user.role == 'admin' || user.role == 'super-admin') {
+        return <Outlet />;
     }
-    
-    return <Outlet />;
+
+    return <Navigate to="/" />;
 }
 
 export default AdminProtectedRoutes;
